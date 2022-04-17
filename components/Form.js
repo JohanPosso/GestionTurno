cb988e2abb1959cbb20487c6f4d09d20fc6c143d
Johan Posso <johan.posito@gmail.com>
2022-04-17 14:11:32 -0500

    Cambios URL 2

--- components/Form.js
+++ components/Form.js
@@ -28,7 +28,7 @@ const Form = ({ selectedLocation }) => {
 
     var params = {
       key: api_key,
-      url: `https://gestion-turnos-p0kkro6nb-johanposso.vercel.app/pdf?nombre=${name
+      url: `https://gestion-turnos.vercel.app/pdf?nombre=${name
         ?.concat(' ')
         ?.concat(subName)
         ?.concat(' ')
